266943e4de1ddaa2e5dac3c7beb0c63cb7f2356c
Florian PRUNIER <florian.prunier@etudiant.univ-lille1.fr>
2018-09-05 13:59:10 +0200

    first etape mouton

--- src/sprint1/Mouton.java
+++ src/sprint1/Mouton.java
@@ -2,8 +2,35 @@ package sprint1;
 
 public class Mouton {
 
+	private boolean estMalade;
+	
 	public Mouton() {
-		// TODO Auto-generated constructor stub
+		estMalade = false;
+	}
+	
+	
+	
+	public void setMalade(boolean estMalade) {
+		this.estMalade = estMalade;
+	}
+	
+	public boolean isMalade() {
+		return estMalade;
+	}
+	
+	@Override
+	public String toString() {
+		String res =
+				"        __  _\n" + 
+				"       .-.'  `; `-._  __  _\n" + 
+				"      (_,         .-:'  `; `-._\n" + 
+				"    ,'o\"(        (_,           )\n" + 
+				"   (__,-'      ,'o\"(            )>\n" + 
+				"      (       (__,-'            )\n" + 
+				"       `-'._.--._(             )\n" + 
+				"          |||  |||`-'._.--._.-'\n" + 
+				"                     |||  |||\n";
+		return res;
 	}
 
 }
