d146de9417efe690ba794c197d4e844bd07d1906
Florian PRUNIER <florian.prunier@etudiant.univ-lille1.fr>
2018-09-05 14:21:25 +0200

    String mouton

--- src/sprint1/TroupeauMouton.java
+++ src/sprint1/TroupeauMouton.java
@@ -4,41 +4,49 @@ public class TroupeauMouton {
 
 	private int nbMalade;
 	private int nbMouton;
-	
+
 	public TroupeauMouton(int nbMouton) {
 		nbMalade = 0;
 		this.nbMouton = nbMouton;
 	}
-	
+
 	public int getNbMalade() {
 		return nbMalade;
 	}
-	
+
 	public int getNbMouton() {
 		return nbMouton;
 	}
-	
+
 	public void setNbMalade(int nbMalade) {
 		this.nbMalade = nbMalade;
 	}
-	
+
 	public void setNbMouton(int nbMouton) {
 		this.nbMouton = nbMouton;
 	}
-	
+
 	@Override
 	public String toString() {
-		String res =
-				"        __  _\n" + 
-				"       .-.'  `; `-._  __  _\n" + 
-				"      (_,         .-:'  `; `-._\n" + 
-				"    ,'o\"(        (_,           )\n" + 
-				"   (__,-'      ,'o\"(            )>\n" + 
-				"      (       (__,-'            )\n" + 
-				"       `-'._.--._(             )\n" + 
-				"          |||  |||`-'._.--._.-'\n" + 
-				"                     |||  |||\n";
+		String res = "";
+		int nbm = this.nbMouton;
+		while (nbm > 0) {
+			for (int i = 0; i < 10 && nbm > 0; i++) {
+				res += "  ,---@> ";
+			}
+			res += "\n";
+			nbm = this.nbMouton;
+			for (int i = 0; i < 10  && nbm > 0; i++) {
+				res += "   W-W'  ";
+			}
+		}
 		return res;
+
+	}
+
+	public static void main(String[] args) {
+		TroupeauMouton tm = new TroupeauMouton(11);
+		System.out.println(tm.toString());
 	}
 
 }
