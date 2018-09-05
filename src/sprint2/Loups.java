f6a618f57d00536a3d6b70d4e82e34c0fbf7c659
Florian PRUNIER <florian.prunier@etudiant.univ-lille1.fr>
2018-09-05 17:15:13 +0200

    Aleas

--- src/sprint2/Loups.java
+++ src/sprint2/Loups.java
@@ -12,7 +12,7 @@ public class Loups extends ConsequenceAlea {
 	public void effectuerAlea(TroupeauMouton moutons, Berger b) {
 		System.out.println(Texts.getAleaLoups());
 
-		System.out.println("Voulez-vous proteger vos moutons\n" + "1- Oui\n" + "2- Non\n");
+		System.out.println("Voulez-vous rentrer vos moutons dans la bergerie ?\n" + "1- Oui\n" + "2- Non\n");
 		Scanner sc = new Scanner(System.in);
 		String res = sc.nextLine();
 		while (!res.matches("[1-2]")) {
@@ -20,17 +20,30 @@ public class Loups extends ConsequenceAlea {
 		}
 		sc.close();
 		if (res.equals("2")) {
-			int nbMoutonMort = moutons.getNbMouton() / 20;
-			System.out.println("Les loups ont réussi à dévorer " + nbMoutonMort + " délicieux moutons...");
-			moutons.setNbMouton(moutons.getNbMouton() - nbMoutonMort);
-			if (moutons.getNbMalade() > moutons.getNbMouton()) {
-				moutons.setNbMalade(moutons.getNbMouton());
+			System.out.println("Voulez-vous pourchassez les loups pour esperer sauvez des moutons ?\n" + "1- Oui\n" + "2- Non\n");
+			Scanner sc2 = new Scanner(System.in);
+			String res2 = sc.nextLine();
+			while (!res2.matches("[1-2]")) {
+				res2 = sc2.nextLine();
 			}
-		}else {
-			int fatigue = b.getJaugeFatigue()-3;
-			System.out.println("Votre jauge de fatigue passe a "+fatigue);
+			sc2.close();
+			if (res2.equals("2")) {
+				int nbMoutonMort = moutons.getNbMouton() / 20;
+				System.out.println("Les loups ont réussi à dévorer " + nbMoutonMort + " délicieu(x) mouton(s)...");
+				moutons.setNbMouton(moutons.getNbMouton() - nbMoutonMort);
+				if (moutons.getNbMalade() > moutons.getNbMouton()) {
+					moutons.setNbMalade(moutons.getNbMouton());
+				}
+			} else {
+				int fatigue = b.getJaugeFatigue() - 4;
+				System.out.println("Votre jauge de fatigue passe a " + fatigue);
+				b.setJaugeFatigue(fatigue);
+			}
+		} else {
+			int fatigue = b.getJaugeFatigue() - 3;
+			System.out.println("Votre jauge de fatigue passe a " + fatigue);
 			b.setJaugeFatigue(fatigue);
 		}
 	}
-	
+
 }
--- src/sprint2/Tempete.java
+++ src/sprint2/Tempete.java
@@ -12,7 +12,7 @@ public class Tempete extends ConsequenceAlea {
 	public void effectuerAlea(TroupeauMouton moutons, Berger b) {
 		System.out.println(Texts.getAleaTempete());
 
-		System.out.println("Voulez-vous proteger vos moutons\n" + "1- Oui\n" + "2- Non\n");
+		System.out.println("Voulez-vous rentrer vos moutons dans la bergerie ?\n" + "1- Oui\n" + "2- Non\n");
 		Scanner sc = new Scanner(System.in);
 		String res = sc.nextLine();
 		while (!res.matches("[1-2]")) {
@@ -22,7 +22,7 @@ public class Tempete extends ConsequenceAlea {
 		if (res.equals("2")) {
 			int nbMoutonMort = moutons.getNbMouton() / 33;
 			System.out.println("Mince, vous n'avez pas réussi à sauver tous les moutons !\n" + nbMoutonMort
-					+ " moutons sont morts dans la tempête...");
+					+ " mouton(s) est/sont mort(s) dans la tempête...");
 			moutons.setNbMouton(moutons.getNbMouton() - nbMoutonMort);
 			if (moutons.getNbMalade() > moutons.getNbMouton()) {
 				moutons.setNbMalade(moutons.getNbMouton());
@@ -33,5 +33,5 @@ public class Tempete extends ConsequenceAlea {
 			b.setJaugeFatigue(fatigue);
 		}
 	}
-
+	
 }
