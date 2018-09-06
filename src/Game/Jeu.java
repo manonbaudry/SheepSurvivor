fa7c3b089afe21d3c1a21afcebbf2a2b2ec06a11
Florian PRUNIER <florian.prunier@etudiant.univ-lille1.fr>
2018-09-06 14:50:54 +0200

    imp menu

--- src/Game/Jeu.java
+++ src/Game/Jeu.java
@@ -3,7 +3,7 @@
  * 05/09/2018
  */
 package Game;
-import java.awt.event.MouseAdapter;
+
 import java.util.Scanner;
 
 /**
@@ -12,25 +12,46 @@ import java.util.Scanner;
  */
 public class Jeu {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		//intro
+	public void menu() {
 		System.out.println(Texts.moutonIntro());
 		System.out.println(Texts.getIntro());
-		System.out.println(Texts.getRegles());
+		System.out.println("\n");
+		System.out.println("1- Jeu");
+		System.out.println("2- Regle");
+		System.out.println("3- Quitter");
 		
-		
+		Scanner sc = new Scanner(System.in);
+		String res = "";
+		while (!res.matches("[1-3]")) {
+			res = sc.nextLine();
+		}
+		if (res.equals("1")) {
+			Util.cls();
+			run();
+		}else if (res.equals("2")) {
+			Util.cls();
+			System.out.println(Texts.getRegles());
+			System.out.println("\nTapper menu pour revenir");
+			Scanner sc2 = new Scanner(System.in);
+			String res2 = "";
+			while (!res2.matches("menu")) {
+				res2 = sc2.nextLine();
+			}
+			Util.cls();
+			menu();
+		}else {
+			System.exit(0);
+		}
+	}
+
+	public void run() {
 		
 		Partie p = new Partie(new Berger(Partie.saisieNom()), new TroupeauMouton(30));
-		while(! p.isFinPartie()) {
+		while (!p.isFinPartie()) {
 			Util.cls();
 			p.afichageDonnees();
 			if (p.getTroupeau().isEstDansGrange()) {
-				System.out.println("Voulez-vous sortir les moutons dans le pré ?\n"
-						+ "1- Oui\n"
-						+ "2- Non\n");
+				System.out.println("Voulez-vous sortir les moutons dans le pré ?\n" + "1- Oui\n" + "2- Non\n");
 				Scanner sc = new Scanner(System.in);
 				String res = sc.nextLine();
 				while (!res.matches("[1-2]")) {
@@ -41,23 +62,26 @@ public class Jeu {
 				}
 			}
 
-			if (p.getTroupeau().getNbMalade() >  0) {
-				p.getTroupeau().setNbMalade(p.getTroupeau().getNbMalade() + (int)(Math.random()*(p.getTroupeau().getNbMouton()-p.getTroupeau().getNbMalade()) / 3));
-				System.out.println("La maladie s'est propagée ! Il y a désormais " + p.getTroupeau().getNbMalade() + " moutons malades.");
+			if (p.getTroupeau().getNbMalade() > 0) {
+				p.getTroupeau().setNbMalade(p.getTroupeau().getNbMalade()
+						+ (int) (Math.random() * (p.getTroupeau().getNbMouton() - p.getTroupeau().getNbMalade()) / 3));
+				System.out.println("La maladie s'est propagée ! Il y a désormais " + p.getTroupeau().getNbMalade()
+						+ " moutons malades.");
 				if (p.getB().getJaugeFatigue() >= 5) {
-					System.out.println("Voulez-vous abattre les moutons malades ?(-2 pt)\n"+"1- Oui\n"+" 2- Non\n");
+					System.out.println("Voulez-vous abattre les moutons malades ?(-2 pt)\n" + "1- Oui\n" + " 2- Non\n");
 					Scanner sc = new Scanner(System.in);
 					String rep = "";
-					while (! rep.matches("[1-2]")) {
+					while (!rep.matches("[1-2]")) {
 						rep = sc.nextLine();
 					}
 					if (rep.equals("1")) {
-						System.out.println("Vous avez abattu les " + p.getTroupeau().getNbMalade() + " moutons malades.");
-						p.getB().setJaugeFatigue(p.getB().getJaugeFatigue()-2);
-						p.getTroupeau().setNbMouton(p.getTroupeau().getNbMouton()-p.getTroupeau().getNbMalade());
+						System.out
+								.println("Vous avez abattu les " + p.getTroupeau().getNbMalade() + " moutons malades.");
+						p.getB().setJaugeFatigue(p.getB().getJaugeFatigue() - 2);
+						p.getTroupeau().setNbMouton(p.getTroupeau().getNbMouton() - p.getTroupeau().getNbMalade());
 						p.getTroupeau().setNbMalade(0);
 					}
-				}else {
+				} else {
 					System.out.println(Texts.getBergerFatigue());
 				}
 			}
@@ -66,30 +90,38 @@ public class Jeu {
 			p.incrementeCompteur();
 			p.decrementHerbe();
 			p.variationFaimMouton();
-			if(p.getHerbe()<1) {
-				System.out.println("il n'y a plus d'herbe dans la prairie... Voulez-vous bouger vos moutons ? (-2 pt)\n"+"1- Oui\n"+"2- Non\n");
+			if (p.getHerbe() < 1) {
+				System.out.println("il n'y a plus d'herbe dans la prairie... Voulez-vous bouger vos moutons ? (-2 pt)\n"
+						+ "1- Oui\n" + "2- Non\n");
 				Scanner sc = new Scanner(System.in);
 				String rep = sc.nextLine();
-				while (! rep.matches("[1-2]")) {
+				while (!rep.matches("[1-2]")) {
 					rep = sc.nextLine();
 				}
-				if(rep.equals("1")){
+				if (rep.equals("1")) {
 					System.out.println("Vous avez bougé vos moutons");
 					p.resetHerbe();
-					p.getB().setJaugeFatigue(p.getB().getJaugeFatigue()-2);
-					
-				}else {
+					p.getB().setJaugeFatigue(p.getB().getJaugeFatigue() - 2);
+
+				} else {
 					System.out.println("Vos moutons n'ont plus à manger");
 				}
 			}
 			p.finPartie();
 			Util.delay();
 		}
-		if(p.isGagne()) {
+		if (p.isGagne()) {
 			System.out.println("Bravo tu as sauvé tes moutons !");
-		}else {
+		} else {
 			System.out.println("Vous avez perdu, tous tes moutons sont morts...");
 		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Jeu().menu();
 	}
 
 }
